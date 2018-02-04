81d6995f49ce118254562e2d649a0ec4a08d7055
Pierre-Louis Girard <pl.girard5@gmail.com>
2018-02-04 13:04:10 +0100

    Classe joueur avec deux interfaces

---
+++ Proto-game/Proto-game/Interfaces.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Proto_game
+{
+    public interface IUpdate
+    {
+        void Update(KeyboardState new_Keyboard_Sate, KeyboardState old_Keyboard_Sate);
+    }
+    public interface IDraw
+    {
+        Texture2D texture {get;}
+        void Draw();
+    }
+}
